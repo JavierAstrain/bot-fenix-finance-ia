6057ebc7e1967709fea0b001fe3a1778b83a8ad9
JavierAstrain <j.astrain.import@gmail.com>
2025-07-22 16:28:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-
 import streamlit as st
 import pandas as pd
 import gspread
-from google.oauth2.service_account import Credentials
+import json
 import requests
+from google.oauth2.service_account import Credentials
 
 # --- CARGA CREDENCIALES GOOGLE DESDE SECRETS ---
-creds_dict = st.secrets["GOOGLE_CREDENTIALS"]
+creds_dict = json.loads(st.secrets["GOOGLE_CREDENTIALS"])
 scope = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
 client_gs = gspread.authorize(creds)
